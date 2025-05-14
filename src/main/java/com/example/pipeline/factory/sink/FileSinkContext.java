c18a4987f8afda57604e69080e035160e4eb5e98
ThatNinjaGuy <vishalgautam.tech@gmail.com>
2025-05-15 00:22:35 +0530

    Parquet file is getting generated from JDBC

--- src/main/java/com/example/pipeline/factory/sink/FileSinkContext.java
+++ src/main/java/com/example/pipeline/factory/sink/FileSinkContext.java
@@ -1,4 +1,4 @@
-package com.example.pipeline.factory;
+package com.example.pipeline.factory.sink;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
--- src/main/java/com/example/pipeline/factory/sink/JdbcSinkContext.java
+++ src/main/java/com/example/pipeline/factory/sink/JdbcSinkContext.java
@@ -1,4 +1,4 @@
-package com.example.pipeline.factory;
+package com.example.pipeline.factory.sink;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
